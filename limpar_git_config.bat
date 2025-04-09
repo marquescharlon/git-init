fd79f734089e0594eac389937a2c6ea1c7c6de92
Marquescharlon Santos <marquescharlonrs@gmail.com>
2025-04-09 10:33:58 -0300

    Adicionando delay

--- limpar_git_config.bat
+++ limpar_git_config.bat
@@ -10,6 +10,7 @@ IF %ERRORLEVEL% EQU 0 (
 ) ELSE (
     echo [✖] Falha ao remover o email global ou nao foi encontrado.
 )
+timeout /t 2 >nul
 
 REM Removendo nome global do Git
 git config --global --unset-all user.name
@@ -18,6 +19,7 @@ IF %ERRORLEVEL% EQU 0 (
 ) ELSE (
     echo [✖] Falha ao remover o nome global ou nao foi encontrado.
 )
+timeout /t 2 >nul
 
 REM Removendo credencial armazenada do GitHub
 cmdkey /delete:LegacyGeneric:target=git:https://github.com
@@ -26,6 +28,7 @@ IF %ERRORLEVEL% EQU 0 (
 ) ELSE (
     echo [✖] Falha ao remover a credencial do GitHub ou ela nao existia.
 )
+timeout /t 2 >nul
 
 echo.
 echo Processo finalizado.
