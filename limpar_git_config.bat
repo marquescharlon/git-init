ad784185b4850def7c61f10dfab87b7e1d7ce0c9
Marquescharlon Santos <marquescharlonrs@gmail.com>
2025-04-09 11:05:30 -0300

    Organizando a impressão

--- limpar_git_config.bat
+++ limpar_git_config.bat
@@ -6,27 +6,29 @@ echo.
 REM Removendo email global do Git
 git config --global --unset-all user.email
 IF %ERRORLEVEL% EQU 0 (
-    echo [OK] Email global removido com sucesso.
+    echo 	[OK] Email global removido com sucesso.
 ) ELSE (
-    echo [?] Falha ao remover o email global ou nao foi encontrado.
+    echo 	[?] Falha ao remover o email global ou nao foi encontrado.
 )
 timeout /t 2 >nul
 
 REM Removendo nome global do Git
 git config --global --unset-all user.name
 IF %ERRORLEVEL% EQU 0 (
-    echo [OK] Nome global removido com sucesso.
+    echo 	[OK] Nome global removido com sucesso.
 ) ELSE (
-    echo [?] Falha ao remover o nome global ou nao foi encontrado.
+    echo 	[?] Falha ao remover o nome global ou nao foi encontrado.
 )
 timeout /t 2 >nul
 
 REM Removendo credencial armazenada do GitHub
 cmdkey /delete:LegacyGeneric:target=git:https://github.com
 IF %ERRORLEVEL% EQU 0 (
-    echo [OK] Credencial do GitHub removida com sucesso.
+    echo.
+    echo 	[OK] Credencial do GitHub removida com sucesso.
 ) ELSE (
-    echo [?] Falha ao remover a credencial do GitHub ou ela nao existia.
+    echo.
+    echo 	[?] Falha ao remover a credencial do GitHub ou ela nao existia.
 )
 timeout /t 2 >nul
 
